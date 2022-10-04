74fba697329f8fd74cebedbd9199bddb91ec7447
state303 <state303@dsub.io>
2022-10-05 01:58:57 +0900

    chore: set StringUtil as final class

--- src/main/java/io/dsub/discogs/api/util/PageUtil.java
+++ src/main/java/io/dsub/discogs/api/util/PageUtil.java
@@ -4,9 +4,8 @@ import io.dsub.discogs.api.Constants;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-public class PageUtil {
+public final class PageUtil {
     private PageUtil(){}
-
     public static Pageable getOrDefaultPageable(Pageable pageable) {
         if (pageable == null) {
             return PageRequest.of(Constants.DEFAULT_PAGE_INDEX, Constants.DEFAULT_PAGE_SIZE);
